0a32a1cd10e0a41cc90b0d66b96466aaefc68387
Corentin <>
2022-08-26 15:01:41 +0200

    sample image

--- Home.py
+++ Home.py
@@ -15,7 +15,9 @@ st.markdown(
     It is built using CellPose, Stardist and custom models and image analysis techniques to automatically analyse myopathy histology images.  
     This web application is intended for demonstration purposes only.  
     ### How to use
-    Click on on the corresponding staining analysis on the sidebar, and upload your histology image. Results will be displayed in the main area automatically.
+    Click on on the corresponding staining analysis on the sidebar, and upload your histology image. Results will be displayed in the main area automatically.  
+    For HE Staining analysis you can download this sample image: [HERE](https://www.lbgi.fr/~meyer/SDH_models/sample_he.jpg)  
+    For SDH Staining analysis you can download this sample image: [HERE](https://www.lbgi.fr/~meyer/SDH_models/sample_sdh.png)  
     ### Who and how
     - Creator and Maintainer: [Corentin Meyer, 3rd year PhD Studient in the CSTB Team, ICube - CNRS - Unistra](https://lambda-science.github.io/)
     - Source code for this appliaction is availiable [HERE](https://github.com/lambda-science/HistoQuant-Streamlit)
