69646d7117078891abf370ca96f7306bd8d89b9b
lambda scientist <20109584+lambda-science@users.noreply.github.com>
2022-10-30 01:54:49 +0200

    Home page

--- Home.py
+++ Home.py
@@ -5,29 +5,29 @@ st.set_page_config(
     page_icon="🔬",
 )
 
-st.write("# Welcome to MyoQuant! 👋")
+st.write("# Welcome to MyoQuant demo ! 👋")
 
 st.sidebar.success("Select the corresponding staining analysis above.")
 
 st.markdown(
     """
-# MyoQuant
+# MyoQuant-Streamlit🔬
 
-MyoQuant is a web application for quantifying the number of cells in a histological image.  
-It is built using CellPose, Stardist and custom models and image analysis techniques to automatically analyze myopathy histology images.  
-This web application is intended for demonstration purposes only.
+MyoQuant-Streamlit🔬 is a demo web application to showcase usage of MyoQuant.  
+MyoQuant is a command line tool to quantify pathological feature in histology images.  
+It is built using CellPose, Stardist, custom neural-network models and image analysis techniques to automatically analyze myopathy histology images.  
+This web application is intended for demonstration purposes only.  
 
 ## How to Use
 
-A Streamlit cloud demo instance should be deployed at https://lbgi.fr/MyoQuant/. I am currently working on docker images and tutorial to deploy the application.  
 Once on the demo, click on the corresponding staining analysis on the sidebar, and upload your histology image. Results will be displayed in the main area automatically.  
 For HE Staining analysis, you can download this sample image: [HERE](https://www.lbgi.fr/~meyer/SDH_models/sample_he.jpg)  
 For SDH Staining analysis, you can download this sample image: [HERE](https://www.lbgi.fr/~meyer/SDH_models/sample_sdh.jpg)
 
 ## Who and how
 
-- Creator and Maintainer: [Corentin Meyer, 3rd year PhD Student in the CSTB Team, ICube—CNRS—Unistra] (https://lambda-science.github.io/)
-- The source code for this application is available [HERE] (https://github.com/lambda-science/MyoQuant)
+- Creator and Maintainer: [Corentin Meyer, 3rd year PhD Student in the CSTB Team, ICube — CNRS — Unistra](https://lambda-science.github.io/)
+- The source code for MyoQuant is available [HERE](https://github.com/lambda-science/MyoQuant), for the demo website it is available [HERE](https://github.com/lambda-science/MyoQuant-Streamlit)
 
 """
 )
