3795c794e5e8c836fd9ef7990d91e78785516f80
Corentin <20109584+lambda-science@users.noreply.github.com>
2022-10-30 16:30:53 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,21 +1,24 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="MyoQuant",
+    page_title="MyoQuant-Streamlit",
     page_icon="🔬",
 )
 
-st.write("# Welcome to MyoQuant demo ! 👋")
+st.write("# Welcome to MyoQuant-Streamlit ! 👋")
 
 st.sidebar.success("Select the corresponding staining analysis above.")
 
 st.markdown(
     """
-# MyoQuant-Streamlit🔬
+# MyoQuant-Streamlit🔬 is a demo web interface to showcase the usage of [MyoQuant](https://github.com/lambda-science/MyoQuant). 
 
-MyoQuant-Streamlit🔬 is a demo web application to showcase usage of MyoQuant.  
-MyoQuant is a command line tool to quantify pathological feature in histology images.  
-It is built using CellPose, Stardist, custom neural-network models and image analysis techniques to automatically analyze myopathy histology images.  
+<p align="center">
+  <img src="https://i.imgur.com/mzALgZL.png" alt="IMPatienT Banner" style="border-radius: 25px;" />
+</p>
+
+[MyoQuant🔬 is a command-line tool to automatically quantify pathological features in muscle fiber histology images.](https://github.com/lambda-science/MyoQuant)
+It is built using CellPose, Stardist, custom neural-network models and image analysis techniques to automatically analyze myopathy histology images. Currently MyoQuant is capable of quantifying centralization of nuclei in muscle fiber with HE staining and anomaly in the mitochondria distribution in muscle fibers with SDH staining.  
 This web application is intended for demonstration purposes only.  
 
 ## How to Use
@@ -24,10 +27,16 @@ Once on the demo, click on the corresponding staining analysis on the sidebar, a
 For HE Staining analysis, you can download this sample image: [HERE](https://www.lbgi.fr/~meyer/SDH_models/sample_he.jpg)  
 For SDH Staining analysis, you can download this sample image: [HERE](https://www.lbgi.fr/~meyer/SDH_models/sample_sdh.jpg)
 
-## Who and how
+## Contact
+Creator and Maintainer: [**Corentin Meyer**, 3rd year PhD Student in the CSTB Team, ICube — CNRS — Unistra](https://lambda-science.github.io/)  <corentin.meyer@etu.unistra.fr>  
+The source code for MyoQuant is available [HERE](https://github.com/lambda-science/MyoQuant), for the demo website (MyoQuant-Streamlit) it is available [HERE](https://github.com/lambda-science/MyoQuant-Streamlit)
 
-- Creator and Maintainer: [Corentin Meyer, 3rd year PhD Student in the CSTB Team, ICube — CNRS — Unistra](https://lambda-science.github.io/)
-- The source code for MyoQuant is available [HERE](https://github.com/lambda-science/MyoQuant), for the demo website it is available [HERE](https://github.com/lambda-science/MyoQuant-Streamlit)
+## Partners
 
+<p align="center">
+  <img src="https://i.imgur.com/m5OGthE.png" alt="Partner Banner" style="border-radius: 25px;" />
+</p>
+
+IMPatienT is born within the collaboration between the [CSTB Team @ ICube](https://cstb.icube.unistra.fr/en/index.php/Home) led by Julie D. Thompson, the [Morphological Unit of the Institute of Myology of Paris](https://www.institut-myologie.org/en/recherche-2/neuromuscular-investigation-center/morphological-unit/) led by Teresinha Evangelista, the [imagery platform MyoImage of Center of Research in Myology](https://recherche-myologie.fr/technologies/myoimage/) led by Bruno Cadot, [the photonic microscopy platform of the IGMBC](https://www.igbmc.fr/en/plateformes-technologiques/photonic-microscopy) led by Bertrand Vernay and the [Pathophysiology of neuromuscular diseases team @ IGBMC](https://www.igbmc.fr/en/igbmc/a-propos-de-ligbmc/directory/jocelyn-laporte) led by Jocelyn Laporte  
 """
 )
